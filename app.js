7f684572f2343b0f77e7eada78e12d6f4cb8abc1
Shashank K Chaudhary <shashank.k.chaudhary@gmail.com>
2022-06-24 15:09:46 +0530

    app.js update

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
 const express = require('express');
 const app = express();
 const env = require('dotenv').config();
-const { v4: uuidv4 } = require('uuid');
 const session = require('express-session');
 
 const connection = require('./db/conn');
 
-const createOrganizationTable = require('./db/table/organization');
-const createUserTable = require('./db/table/user');
 const auth = require('./routes/auth');
 
-const facebookAuth = require('./auth/facebookAuth');
-const googleAuth = require('./auth/googleAuth');
 const passport = require('passport');
-const saveUser = require('./controller/saveUser');
-const { reset } = require('nodemon');
+const isLoggedIn = require('./middleware/isLoggedIn');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
-passport.session();
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use('/auth', auth)
+app.use('/api/auth', auth)
+app.use('/api/organizations', isLoggedIn, require('./routes/organization'));
 
 const port = process.env.PORT || 8080;
 
@@ -37,41 +33,8 @@ connection.connect(err => {
 });
 
 
-//get all users
-app.get('/api/users', (req, res) => {
-    connection.query('SELECT * FROM users', (err, results) => {
-        if (err) return console.log(err);
-        console.log(req)
-        res.json({"result" : results, "get" : "something"});
-    }
-    );
-}
-);
-
-
-//create new user
-app.post('/api/users', async (req, res) => {
-    try{
-    const {name} = await saveUser(connection, uuidv4(), req.body.name, req.body.email, req.body.authSvc);
-    res.json({"result" : name, "post" : "something", "user" : req.user.id});
-    }
-    catch(err){
-        console.log(err);
-    }
-}
-);
-
-
-app.post('/api/organizations', (req, res) => {
-    createOrganizationTable(connection);
-    const { name, user } = req.body;
-    const id = uuidv4();
-    connection.query('INSERT INTO organizations (id, name) VALUES (?, ?)', [id, name], (err, results) => {
-        if (err) return console.log(err);
-        res.json(results);
-    }
-    );
-    createUserOrgRelation(connection);
+app.get('/logged', isLoggedIn, (req, res) => {
+    res.send('yes');
 }
 );
 
